68e34d4ecbf07954990c8555000d88295ab89b58
hongxiaoqiang <cool.hxq@gmail.com>
2019-03-09 00:18:10 +0800

    测试map查找find和count

---
+++ stl/find_or_count_performance/main.cpp
@@ -0,0 +1,33 @@
+#include "../../performance_kit.h"
+#include <map>
+
+int main()
+{
+    std::map<int,int> a;
+
+    for (int i = 0; i < 1000000; ++i)
+    {
+        a.insert(std::make_pair(i,i));
+    }
+
+    {
+        int b = 0;
+        auto f1 = [&]()
+        {
+            auto it = a.find(555);
+            if (it != a.end()){
+                b = it->second;
+            }
+        };
+
+        auto f2 = [&]()
+        {
+            if (a.count(555)) {
+                b = a[555];
+            }
+        };
+
+        CALL_TIMES_AND_TIME(1000000, "11", f1()) // 126 毫秒
+        CALL_TIMES_AND_TIME(1000000, "22", f2()) // 225 毫秒
+    }
+}
